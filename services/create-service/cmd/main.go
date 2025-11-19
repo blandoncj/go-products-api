8ef24f1750e9d84d1c180d23b8d4a3ba5d5b3c61
Jacobo Blandón Castro <jacoboblandon94@gmail.com>
2025-11-18 19:21:14 -0500

    feat(create-service): add Dockerfile and HTTP server
    Add Dockerfile for containerizing the create-service. Update main.go to
    start an HTTP server using controller handler and configurable port.

--- services/create-service/cmd/main.go
+++ services/create-service/cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/blandoncj/go-products-api/services/create-service/internal/controller"
+)
 
 func main() {
-	fmt.Println("It works!")
+	port := os.Getenv("CREATE_SERVICE_PORT")
+	if port == "" {
+		port = "8081"
+	}
+
+	handler := controller.NewHandler()
+	addr := fmt.Sprintf(":%s", port)
+	log.Printf("Create service listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, handler))
 }
